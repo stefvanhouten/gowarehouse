5ac4e116b5621269f2e71fc59f2760851342097b
stefvanhouten <stefvanhouten@gmail.com>
2022-11-26 11:25:27 +0100

    use conf.Password instead of os.Getenv.

--- database/database.go
+++ database/database.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"os"
 	"time"
 
 	"example/logger" // Our local logging module.
@@ -17,28 +16,24 @@ type config struct {
 	Database string `env:"DATABASE"`
 }
 
+// Load required environment variables and put them in the config struct.
 func loadEnv() config {
 	logger.DefaultLogger.Info("Loading environment variables.")
+
 	cfg := config{}
 	if err := env.Parse(&cfg); err != nil {
 		panic(err.Error())
 	}
+
 	return cfg
 }
 
-func main() {
-	conf := loadEnv()
-
-	// Log the user and database we are attempting to connect to.
-	logger.DefaultLogger.WithFields(logrus.Fields{
-		"user":     conf.User,
-		"database": conf.Database,
-	}).Debug("Setting up connection to database.")
-
+// Connect to the MYSQL database and return the connection.
+func connect(conf *config) *sql.DB {
 	// Setup a database CONFIG with User/Passwd sourced from environment variables.
 	cfg := mysql.Config{
 		User:                 conf.User,
-		Passwd:               os.Getenv("DBPASSWORD"),
+		Passwd:               conf.Password,
 		Net:                  "tcp",
 		Addr:                 "localhost:3306",
 		DBName:               conf.Database,
@@ -66,6 +61,19 @@ func main() {
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(10)
+
+	return db
+}
+
+func main() {
+	conf := loadEnv()
+	connect(&conf)
+
+	// Log the user and database we are attempting to connect to.
+	logger.DefaultLogger.WithFields(logrus.Fields{
+		"user":     conf.User,
+		"database": conf.Database,
+	}).Debug("Setting up connection to database.")
 
 	logger.DefaultLogger.Info("Connected to database.")
 }
