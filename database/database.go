aee6b4d1d8a3317b2ea0740729cdbb75ff1d2107
stefvanhouten <stefvanhouten@gmail.com>
2022-11-26 11:04:42 +0100

    Load environment variables into struct when go launches.

--- database/database.go
+++ database/database.go
@@ -5,27 +5,43 @@ import (
 	"os"
 	"time"
 
-	"example/logger"
+	"example/logger" // Our local logging module.
+	"github.com/caarlos0/env/v6"
 	"github.com/go-sql-driver/mysql"
 	"github.com/sirupsen/logrus"
 )
 
-func main() {
-	user := os.Getenv("DBUSER")
-	database := "gowarehouse"
+type config struct {
+	User     string `env:"DBUSER"`
+	Password string `env:"DBPASSWORD"`
+	Database string `env:"DATABASE"`
+}
 
+func loadEnv() config {
+	logger.DefaultLogger.Info("Loading environment variables.")
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		panic(err.Error())
+	}
+	return cfg
+}
+
+func main() {
+	conf := loadEnv()
+
+	// Log the user and database we are attempting to connect to.
 	logger.DefaultLogger.WithFields(logrus.Fields{
-		"user":     user,
-		"database": database,
+		"user":     conf.User,
+		"database": conf.Database,
 	}).Debug("Setting up connection to database.")
 
 	// Setup a database CONFIG with User/Passwd sourced from environment variables.
 	cfg := mysql.Config{
-		User:                 user,
+		User:                 conf.User,
 		Passwd:               os.Getenv("DBPASSWORD"),
 		Net:                  "tcp",
 		Addr:                 "localhost:3306",
-		DBName:               database,
+		DBName:               conf.Database,
 		AllowNativePasswords: true,
 	}
 
@@ -41,13 +57,12 @@ func main() {
 	err = db.Ping()
 	if err != nil {
 		logger.DefaultLogger.WithFields(logrus.Fields{
-			"user":     user,
-			"database": database,
+			"user":     conf.User,
+			"database": conf.Database,
 		}).Error("Error connecting to database:", err.Error())
 		panic(err.Error())
 	}
 
-	//
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(10)
